e552730a0ce3e481f8d8777825f32500c18fa769
ben <s3729184@student.rmit.edu.au>
2021-04-09 15:14:33 +1000

    edit syntax

--- scripts/e2e-ci.sh
+++ scripts/e2e-ci.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo docker build -f src/qawolfdockerfile ./src -t qabuild:latest --no-cache
-sudo docker-compose -f docker-compose-e2e.yml up -d
-sudo docker exec -it qawolf /usr/app/e2e.sh
+docker build -f src/qawolfdockerfile ./src -t qabuild:latest --no-cache
+docker-compose -f docker-compose-e2e.yml up -d
+docker exec -it qawolf /usr/app/e2e.sh
